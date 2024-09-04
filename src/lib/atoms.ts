744a96e66522270c3e75ce9edbbe9f75d67e94d8
utkuonursahin <sahinutkuonur@gmail.com>
2024-09-04 11:53:51 +0300

    feat: convert currency selection to combobox

--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -1,9 +1,10 @@
 import {atom} from "jotai";
 import {ConversionState} from "@/constants/ConversionState";
+import {Currency} from "@/constants/Currency";
 
 export const currencyConversionAtom = atom<ConversionState>({
-    from: 'USD',
-    to: 'EUR',
+    from: {name: 'US Dollar', symbol: '$', code: 'USD'} as Currency,
+    to: {name: 'Euro', symbol: '€', code: 'EUR'} as Currency,
     amount: 1,
     convertedAmount: 0,
     historicalData: []
